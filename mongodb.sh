c4fa258c188bf02545163f1054f96944b28fc762
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-04-25 21:14:33 -0500

    Commit

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,6 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
-sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+#sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 systemctl enable mongod
 systemctl start mongod
+systemctl restart mongod
