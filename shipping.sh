1df0e420442c4da7e3c174751211f6fc76eee24a
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-04-26 20:39:16 -0500

    Commit

--- shipping.sh
+++ shipping.sh
@@ -16,9 +16,9 @@ systemctl enable shipping
 systemctl start shipping
 
 dnf install mysql -y
-mysql -h <mysql-dev.rahuldevops85.online> -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h <mysql-dev.rahuldevops85.online> -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h <mysql-dev.rahuldevops85.online> -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql-dev.rahuldevops85.online -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql-dev.rahuldevops85.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+mysql -h mysql-dev.rahuldevops85.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 
 systemctl daemon-reload
