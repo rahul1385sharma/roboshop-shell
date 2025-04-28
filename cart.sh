84a40b6d1f32c8a4066769e043de15d82efc620b
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-04-28 01:45:03 -0500

    Commit

--- cart.sh
+++ cart.sh
@@ -3,15 +3,6 @@ source common.sh
 #function call
 nodejs
 
-cp cart.service /etc/systemd/system/cart.service
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-cd /app
-unzip /tmp/cart.zip
-
-cd /app
-npm install
 #function call
 system_setup
 
--- common.sh
+++ common.sh
@@ -6,7 +6,18 @@ systemctl start ${service_name}
 
 nodejs()
 {
+  cp cart.service /etc/systemd/system/${service_name}.service
   dnf module disable nodejs -y
   dnf module enable nodejs:20 -y
   dnf install nodejs -y
+  useradd roboshop
+  rm -rf /app
+  mkdir /app
+  cd /app
+  npm install
+  curl -L -o /tmp/${service_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${service_name}-v3.zip
+  cd /app
+  unzip /tmp/${service_name}.zip
+  cd /app
+  npm install
 }
