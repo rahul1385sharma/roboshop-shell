bfea4b6ebbaf27e99857d95bb39cdb1ae448e4d7
rahul1385sharma <rahul.1385.sharma@gmail.com>
2025-04-28 01:47:53 -0500

    Commit

--- catalogue.sh
+++ catalogue.sh
@@ -3,18 +3,9 @@ source common.sh
 #function call
 nodejs
 
-cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-
-useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-#function call
-system_setup
-
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.rahuldevops85.online </app/db/master-data.js
+
+#function call
+system_setup
